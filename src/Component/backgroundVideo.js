14476d57c0cb6b1a60f9a0cd5ccbf1fe0b0c91ce
Lee-Sang-Beom <tkdqja975@naver.com>
2022-09-25 20:17:24 +0900

    replace video main

--- src/Component/backgroundVideo.js
+++ src/Component/backgroundVideo.js
@@ -1,15 +1,22 @@
-import Ori from "../video/Ori_and_the_Blind_Forest.mp4";
+import Ori from "../video/main.mp4";
 import styles from "../css/background/MainBackground.module.css";
 
-export default function BackgroundVideo(){
-    return (
-        <>
-        <video
-            autoPlay loop muted
-            className={styles.background}
-        >
-            <source src={Ori} type="video/mp4"/>
-        </video>
-        </>
-    )
+export default function BackgroundVideo() {
+  return (
+    <>
+      <video autoPlay loop muted className={styles.background}>
+        <source src={Ori} type="video/mp4" />
+      </video>
+
+      {/* <div class="video">
+        <iframe
+          src="https://www.youtube.com/embed/ClCW2A1355A"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+      </div> */}
+    </>
+  );
 }
