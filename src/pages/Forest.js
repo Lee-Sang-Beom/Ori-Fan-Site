616cf70e65846a23a2d9ec5c9afd01ad5c8bf751
Lee-Sang-Beom <tkdqja975@naver.com>
2022-09-30 19:38:53 +0900

    install scroll-out, test operate

--- src/pages/Forest.js
+++ src/pages/Forest.js
@@ -1,8 +1,125 @@
+import styles from "../css/forest/Forest.module.css";
+import ScrollOut from "scroll-out";
+import img_1 from "../img/1.jpg";
+
 function Forest() {
+
+  ScrollOut({
+    /* options */
+  });
+
   return (
-      <div>
-        hello!!!!!!!!!!!!!!!
+    <div className={styles.container}>
+      <div className={styles.site_wrap}>
+        <section className={styles.section} data-scroll>
+          <div className={styles.box}>
+            <div className={styles.left_box}>
+              <img src={img_1} alt="img1" />
+            </div>
+            <div className={styles.right_box}>
+              <h1>Scroll in down</h1>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+                ut dolor nisl. Nunc hendrerit tempus tortor in imperdiet. Sed
+                maximus neque non euismod lobortis. className aptent taciti
+                sociosqu ad litora torquent per conubia nostra, per inceptos
+                himenaeos. Nunc a magna a ex vestibulum cursus eget vel libero.
+                Aliquam mollis magna diam, et tempor ipsum finibus vitae. Aenean
+                ac urna ornare, faucibus orci mattis, condimentum enim. Fusce
+                porta est eget finibus placerat. Proin in sapien quis eros
+                posuere volutpat ac id lacus. Pellentesque fringilla viverra mi,
+                sed ultricies neque scelerisque id. Aenean lobortis pulvinar
+                ultrices. Nunc tincidunt, tellus non iaculis varius, velit
+                sapien mattis ex, quis rhoncus lacus urna vehicula quam.
+              </p>
+            </div>
+          </div>
+        </section>
       </div>
+
+      <div className={styles.site_wrap}>
+        <section className={styles.section} data-scroll>
+          <div className={styles.box}>
+            <div className={styles.left_box}>
+              <img src={img_1} alt="img1" />
+            </div>
+            <div className={styles.right_box}>
+              <h1>Scroll in down</h1>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+                ut dolor nisl. Nunc hendrerit tempus tortor in imperdiet. Sed
+                maximus neque non euismod lobortis. className aptent taciti
+                sociosqu ad litora torquent per conubia nostra, per inceptos
+                himenaeos. Nunc a magna a ex vestibulum cursus eget vel libero.
+                Aliquam mollis magna diam, et tempor ipsum finibus vitae. Aenean
+                ac urna ornare, faucibus orci mattis, condimentum enim. Fusce
+                porta est eget finibus placerat. Proin in sapien quis eros
+                posuere volutpat ac id lacus. Pellentesque fringilla viverra mi,
+                sed ultricies neque scelerisque id. Aenean lobortis pulvinar
+                ultrices. Nunc tincidunt, tellus non iaculis varius, velit
+                sapien mattis ex, quis rhoncus lacus urna vehicula quam.
+              </p>
+            </div>
+          </div>
+        </section>
+      </div>
+
+      <div className={styles.site_wrap}>
+        <section className={styles.section} data-scroll>
+          <div className={styles.box}>
+            <div className={styles.left_box}>
+              <img src={img_1} alt="img1" />
+            </div>
+            <div className={styles.right_box}>
+              <h1>Scroll in down</h1>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+                ut dolor nisl. Nunc hendrerit tempus tortor in imperdiet. Sed
+                maximus neque non euismod lobortis. className aptent taciti
+                sociosqu ad litora torquent per conubia nostra, per inceptos
+                himenaeos. Nunc a magna a ex vestibulum cursus eget vel libero.
+                Aliquam mollis magna diam, et tempor ipsum finibus vitae. Aenean
+                ac urna ornare, faucibus orci mattis, condimentum enim. Fusce
+                porta est eget finibus placerat. Proin in sapien quis eros
+                posuere volutpat ac id lacus. Pellentesque fringilla viverra mi,
+                sed ultricies neque scelerisque id. Aenean lobortis pulvinar
+                ultrices. Nunc tincidunt, tellus non iaculis varius, velit
+                sapien mattis ex, quis rhoncus lacus urna vehicula quam.
+              </p>
+            </div>
+          </div>
+        </section>
+      </div>
+
+      <div className={styles.site_wrap}>
+        <section className={styles.section} data-scroll>
+          <div className={styles.box}>
+            <div className={styles.left_box}>
+              <img src={img_1} alt="img1" />
+            </div>
+            <div className={styles.right_box}>
+              <h1>Scroll in down</h1>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+                ut dolor nisl. Nunc hendrerit tempus tortor in imperdiet. Sed
+                maximus neque non euismod lobortis. className aptent taciti
+                sociosqu ad litora torquent per conubia nostra, per inceptos
+                himenaeos. Nunc a magna a ex vestibulum cursus eget vel libero.
+                Aliquam mollis magna diam, et tempor ipsum finibus vitae. Aenean
+                ac urna ornare, faucibus orci mattis, condimentum enim. Fusce
+                porta est eget finibus placerat. Proin in sapien quis eros
+                posuere volutpat ac id lacus. Pellentesque fringilla viverra mi,
+                sed ultricies neque scelerisque id. Aenean lobortis pulvinar
+                ultrices. Nunc tincidunt, tellus non iaculis varius, velit
+                sapien mattis ex, quis rhoncus lacus urna vehicula quam.
+              </p>
+            </div>
+          </div>
+        </section>
+      </div>
+
+      {/* 2 */}
+    </div>
   );
 }
 
