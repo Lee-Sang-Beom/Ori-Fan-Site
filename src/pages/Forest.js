6547c217a03f580f6c6daf6769809071b0111e42
Lee-Sang-Beom <tkdqja975@naver.com>
2022-10-03 22:14:52 +0900

    add img, notify img step, add img media query

--- src/pages/Forest.js
+++ src/pages/Forest.js
@@ -4,9 +4,9 @@ import forest_start from "../img/forest_start.jpg";
 import tree from "../img/tree.jpg";
 import wind from "../img/wind.jpg";
 import mountain from "../img/mountain.jpg";
-import top_icon from "../img/top_icon.png";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faDownLong } from "@fortawesome/free-solid-svg-icons";
+import top_icon from "../img/top_icon.jpg";
+import forest_end from "../img/forest_end.jpg";
+
 
 import { useEffect } from "react";
 
@@ -17,15 +17,12 @@ function Forest() {
     });
   }, []);
 
-  
-
   return (
     <div className={styles.container}>
       <div className={styles.site_wrap}>
-        <div  className={styles.forest_logo}>
+        <div className={styles.forest_logo}>
           <img src={top_icon}/>
         </div>
-
         {/* 1 */}
         <div className={styles.image1}></div>
         <section data-scroll className={styles.content}>
@@ -34,7 +31,7 @@ function Forest() {
               <img src={forest_start} alt="img1" />
             </div>
             <div className={styles.right_box}>
-              <h1>모험의 시작</h1>
+              <h1>시작</h1>
               <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
                 ut dolor nisl. Nunc hendrerit tempus tortor in imperdiet. Sed
@@ -116,6 +113,33 @@ function Forest() {
             </div>
             <div className={styles.right_box}>
               <h1>호루산</h1>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+                ut dolor nisl. Nunc hendrerit tempus tortor in imperdiet. Sed
+                maximus neque non euismod lobortis. Class aptent taciti sociosqu
+                ad litora torquent per conubia nostra, per inceptos himenaeos.
+                Nunc a magna a ex vestibulum cursus eget vel libero. Aliquam
+                mollis magna diam, et tempor ipsum finibus vitae. Aenean ac urna
+                ornare, faucibus orci mattis, condimentum enim. Fusce porta est
+                eget finibus placerat. Proin in sapien quis eros posuere
+                volutpat ac id lacus. Pellentesque fringilla viverra mi, sed
+                ultricies neque scelerisque id. Aenean lobortis pulvinar
+                ultrices. Nunc tincidunt, tellus non iaculis varius, velit
+                sapien mattis ex, quis rhoncus lacus urna vehicula quam.
+              </p>
+            </div>
+          </div>
+        </section>
+
+        {/* 5 */}
+        <div className={styles.image5}></div>
+        <section data-scroll className={styles.content}>
+          <div className={styles.box}>
+            <div className={styles.left_box}>
+              <img src={forest_end} alt="img1" />
+            </div>
+            <div className={styles.right_box}>
+              <h1>끝, 그리고 새로운 시작</h1>
               <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
                 ut dolor nisl. Nunc hendrerit tempus tortor in imperdiet. Sed
