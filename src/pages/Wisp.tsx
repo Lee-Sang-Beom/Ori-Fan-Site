d8ff9d273b7c8c67a2387ee5e5224d96df98a530
Lee-Sang-Beom <tkdqja975@naver.com>
2022-10-15 18:15:08 +0900

    add img, section content : in wisp page (kuroFeature)

--- src/pages/Wisp.tsx
+++ src/pages/Wisp.tsx
@@ -11,7 +11,7 @@ import topIcon from "../img/wisp_src/topIcon.jpg";
 
 import wispStart from "../img/wisp_src/start.jpg";
 import abnormalWing from "../img/wisp_src/abnormalWings.jpg";
-
+import kuroFeather from "../img/wisp_src/kuroFeather.jpg";
 
 
 function Forest() {
@@ -45,10 +45,20 @@ function Forest() {
           <SectionContent
             imgsrc={abnormalWing}
             title="상실"
-            text="새의 본능에 따라, 쿠는 성장하면서 하늘을 날아다니는 것에 대한 꿈을 꾸게 됩니다. 하지만, 선천적으로 정상적이지 못한 날개때문에 쿠는 상실감에 빠지게 됩니다."
+            text="새의 본능에 따라, 쿠는 성장하면서 하늘을 날아다니는 것에 대한 꿈을 꾸게 됩니다. 하지만, 선천적으로 정상적이지 못한 한 쪽 날개 때문에 쿠는 상실감에 빠지게 됩니다."
           />
         </section>
 
+        {/* 3 */}
+        <div className={styles.image2}></div>
+        <section data-scroll className={styles.content}>
+          <SectionContent
+            imgsrc={kuroFeather}
+            title="어머니의 선물"
+            text="Ori는 쿠로의 깃털을 이용하면, 쿠가 날 수 있을 것이라 생각하여 구모를 찾아갑니다. 
+                구모는 Ori의 의중을 이해하고, 쿠의 비정상적인 날개에 쿠로의 깃털을 묶어주게 됩니다."
+          />
+        </section>
        
       </div>
     </div>
